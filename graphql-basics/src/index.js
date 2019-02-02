bd7f7922e604807653486b3b00605afe725b9d60
aitorarias <aitor.acruz@gmail.com>
2019-02-02 16:39:34 +0100

    Updating logic in graphQL 🔥

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,36 @@
+import {
+  GraphQLServer,
+  PubSub
+} from "graphql-yoga";
+import db from "./db";
+
+import Query from "./resolvers/Query";
+import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription"
+import Post from "./resolvers/Post";
+import Comment from "./resolvers/Comment";
+import User from "./resolvers/User";
+
+const pubsub = new PubSub();
+
+// Running the server
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers: {
+    Query,
+    Mutation,
+    Subscription,
+    Post,
+    User,
+    Comment
+  },
+  context: {
+    db,
+    pubsub
+  }
+});
+
+// On localhost 4000
+server.start(() => {
+  console.log("localhost:4000 is on fire 🔥 ");
+});
