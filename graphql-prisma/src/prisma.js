9b284266485f54d7103b868cdb7d8e0a5e3a566c
aitorarias <aitor.acruz@gmail.com>
2019-02-05 20:12:59 +0100

    Implemented asynchronous functions in prisma-binding

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -9,52 +9,62 @@ const prisma = new Prisma({
 
 //prisma.query prisma.mutation prisma.subscription prisma.exits
 
+const createPostForUser = async (authorId, data) => {
 
-
-// prisma.query.users(null, '{id name posts {id title} }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// prisma.query.posts(null, '{id title body published}').then((data) =>{
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// prisma.query.comments(null, '{ id text author {id name} }').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// prisma.mutation.createPost({
-//     data: {
-//         title: "Esto es un nuevo post hecho desde prisma-binding",
-//         body: "Prisma binding es lo mejor del mundo",
-//         published: true,
-//         author: {
-//             connect: {
-//                 id: "cjrqc184j000p07682ftn1x41"
-//             }
-//         }
-//     }
-// }, '{id title body published}').then((data) => {
-//     console.log(data)
-//     return prisma.query.users(null, '{id name posts {id title body} }')
-// }).then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// })
-
-// updatePost y cambiar el body y el published al valor contrario marcado
-// Fetch todos los posts (id, title, body, published) e imprimirlo por consola
-// Ver todos los posts y comprobar que todo funciona OK
-
-prisma.mutation.updatePost({
-    where: {
-        id: "cjrrpsjhx02iq0768o6nxr57f"
-    },
-    data: {
-        body: "Modificación del body desde prisma-binding",
-        published: true
-    }
-}, '{id}').then((data) => {
-    return prisma.query.posts(null, '{id title body published}').then((data) => {
-        console.log(JSON.stringify(data, undefined, 2))
+    const userExists = await prisma.exists.User({
+        id: authorId
     })
+
+    if (!userExists) {
+        throw new Error('User not found')
+    }
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: authorId
+                }
+            }
+        }
+    }, '{author {id name email posts {id title published} } }')
+    return post.author
+}
+
+// createPostForUser("cjrrwi7jn03md0768gr83omda", {
+//     title: "Esto es un post con asíncronas",
+//     body: "Las asíncronas son mejores a la hora de traer datos anidados, en vez de hacerlo por continuos callbacks. Un async siempre devuelve algo",
+//     published: true
+// }).then((user) => {
+//     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error.message)
+// })
+
+
+const updatePostForUser = async (postId, data) => {
+    const postExits = await prisma.exists.Post({
+        id: postId
+    })
+
+    if (!postExits) {
+        throw new Error('Post not found')
+    }
+
+    const post = await prisma.mutation.updatePost({
+        where: {
+            id: postId
+        },
+        data
+    }, '{author { id name email posts {id title published } } }')
+
+    return post.author
+}
+
+updatePostForUser("cjrrwtjad03tm07684zfarwpm", {
+    published: false
+}).then((user) => {
+    console.log(JSON.stringify(user, undefined, 2))
+}).catch((error) => {
+    console.log(error.message)
 })
