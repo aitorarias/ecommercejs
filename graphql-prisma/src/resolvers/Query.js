4bb161659211a8ea33097c469e8dd0ab039f1ca4
aitorarias <aitor.acruz@gmail.com>
2019-02-08 20:27:44 +0100

    Added shorting and pagination

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -1,104 +1,128 @@
-import getUserId from "../utils/getUserId";
+import getUserId from '../utils/getUserId'
 
 const Query = {
-  users(parent, args, { prisma }, info) {
-    const opArgs = {};
+  users(parent, args, {
+    prisma
+  }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    }
 
     if (args.query) {
       opArgs.where = {
-        OR: [
-          {
-            name_contains: args.query
-          }
-        ]
-      };
+        OR: [{
+          name_contains: args.query
+        }]
+      }
     }
 
-    return prisma.query.users(opArgs, info);
+    return prisma.query.users(opArgs, info)
   },
-  myPosts(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request);
+  myPosts(parent, args, {
+    prisma,
+    request
+  }, info) {
+    const userId = getUserId(request)
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId
         }
       }
-    };
-
-    if (args.query) {
-      opArgs.where.OR = [
-        {
-          title_contains: args.query
-        },
-        {
-          body_contains: args.query
-        }
-      ];
     }
 
-    return prisma.query.posts(opArgs, info);
+    if (args.query) {
+      opArgs.where.OR = [{
+        title_contains: args.query
+      }, {
+        body_contains: args.query
+      }]
+    }
+
+    return prisma.query.posts(opArgs, info)
   },
-  posts(parent, args, { prisma }, info) {
+  posts(parent, args, {
+    prisma
+  }, info) {
     const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: true
       }
-    };
-
-    if (args.query) {
-      opArgs.where.OR = [
-        {
-          title_contains: args.query
-        },
-        {
-          body_contains: args.query
-        }
-      ];
     }
 
-    return prisma.query.posts(opArgs, info);
+    if (args.query) {
+      opArgs.where.OR = [{
+        title_contains: args.query
+      }, {
+        body_contains: args.query
+      }]
+    }
+
+    return prisma.query.posts(opArgs, info)
   },
-  comments(parent, args, { prisma }, info) {
-    return prisma.query.comments(null, info);
+  comments(parent, args, {
+    prisma
+  }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    }
+
+    return prisma.query.comments(opArgs, info)
   },
-  me(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request);
+  me(parent, args, {
+    prisma,
+    request
+  }, info) {
+    const userId = getUserId(request)
 
     return prisma.query.user({
       where: {
         id: userId
       }
-    });
+    })
   },
-  async post(parent, args, { prisma, request }, info) {
-    const userId = getUserId(request, false);
+  async post(parent, args, {
+    prisma,
+    request
+  }, info) {
+    const userId = getUserId(request, false)
 
-    const posts = await prisma.query.posts(
-      {
-        where: {
-          id: args.id,
-          OR: [
-            {
-              published: true
-            },
-            {
-              author: {
-                id: userId
-              }
-            }
-          ]
-        }
-      },
-      info
-    );
+    const posts = await prisma.query.posts({
+      where: {
+        id: args.id,
+        OR: [{
+          published: true
+        }, {
+          author: {
+            id: userId
+          }
+        }]
+      }
+    }, info)
 
     if (posts.length === 0) {
-      throw new Error("Post not found");
+      throw new Error('Post not found')
     }
 
-    return posts[0];
+    return posts[0]
   }
-};
+}
 
-export { Query as default };
+export {
+  Query as
+  default
+}
